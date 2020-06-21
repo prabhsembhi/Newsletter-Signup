ef599b008fba0da16d7d4ff14c3a3572d8002c4e
prabhsembhi <prabhpreetsingh013@gmail.com>
2020-06-21 00:28:33 -0700

    Added the Auth with API and started saving members

--- app.js
+++ app.js
@@ -7,10 +7,53 @@ const request = require("request");
 
 const app = express();
 
-app.get("/", function(req,res){
-   res.send("waheguru");
-})
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function(req,res){
-    console.log("Server started at port 3000");
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const fName = req.body.fName;
+  const lName = req.body.lName;
+  const email = req.body.email;
+  res.send(" " + fName);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fName,
+          LNAME: lName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const api = "48909a6433026c8d989f5f33ce4e716e-us10";
+  const listID = "3b87265807";
+  const url = "https://us10.api.mailchimp.com/3.0/lists/" + listID;
+
+  const options = {
+    method: "POST",
+    auth: "prabh:" + api,
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(3000, function (req, res) {
+  console.log("Server started at port 3000");
+});
